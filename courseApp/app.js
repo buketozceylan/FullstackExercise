c2d1f7e95b8ddc780947b504fcc95634bf2bbb07
Buket Özceylan <buketozceylan@outlook.com>
2026-01-31 21:42:48 +0300

    Classes added

--- courseApp/app.js
+++ courseApp/app.js
@@ -19,8 +19,8 @@ UI.prototype.addCourseToList = function (course) {
         </td>
         <td>${course.title}</td>
         <td>${course.instructor}</td>
-        <td><a href="#" class="btn btn-danger btn-sm">Delete</a></td>
-    </tr>`
+        <td><a href="#" class="btn btn-danger btn-sm delete">Delete</a></td>
+    </tr>`;
 
     list.innerHTML += html;
 }
@@ -31,6 +31,19 @@ UI.prototype.clearControls = function () {
     const title = document.getElementById('title').value = "";
     const instructor = document.getElementById('instructor').value = "";
     const image = document.getElementById('image').value = "";
+}
+UI.prototype.deleteTarget = function(element){
+    if (element.classList.contains('delete')) {
+        element.parentElement.parentElement.remove();
+    }
+}
+UI.prototype.showAlert = function(message,className){
+    var alert  = `<div class="alert alert-${className}">${message}</div>`;
+    const row = document.querySelector('.row');
+    row.insertAdjacentHTML('beforebegin',alert);
+    setTimeout(()=>{
+        document.querySelector('.alert').remove();
+    },3000);
 }
 
 document.getElementById('new-course').addEventListener('submit', function(e) {
@@ -44,11 +57,26 @@ document.getElementById('new-course').addEventListener('submit', function(e) {
 
 
     const ui = new UI();
-    ui.addCourseToList(course);
 
-    ui.clearControls();
+    if (title === '' || instructor === '' || image === '' ) {
+        ui.showAlert('Please complete the form', 'warning');
+    }else{
+        ui.addCourseToList(course);
+
+        ui.clearControls();
+        ui.showAlert('The course has been added.','success')
+    }
+
+
     
     
     e.preventDefault();
     
+})
+
+document.getElementById("course-list").addEventListener('click', function(e){
+
+    const ui = new UI();
+    ui.deleteTarget(e.target);
+    ui.showAlert('The course has been deleted.', 'success')
 })
