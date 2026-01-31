d6332fd26b5bd4463ac009c7667aa26d86fdf391
Buket Özceylan <buketozceylan@outlook.com>
2026-01-31 20:57:34 +0300

    next

--- courseApp/app.js
+++ courseApp/app.js
@@ -4,6 +4,34 @@ function Course(title,instructor,image){
     this.instructor = instructor;
     this.image = image;
 }
+function UI() {
+    
+}
+
+UI.prototype.addCourseToList = function (course) {
+    
+    const list = document.getElementById('course-list');
+
+    var html = `
+    <tr>
+        <td>
+            <img src="img/${course.image}"/>
+        </td>
+        <td>${course.title}</td>
+        <td>${course.instructor}</td>
+        <td><a href="#" class="btn btn-danger btn-sm">Delete</a></td>
+    </tr>`
+
+    list.innerHTML += html;
+}
+
+
+
+UI.prototype.clearControls = function () {
+    const title = document.getElementById('title').value = "";
+    const instructor = document.getElementById('instructor').value = "";
+    const image = document.getElementById('image').value = "";
+}
 
 document.getElementById('new-course').addEventListener('submit', function(e) {
 
@@ -13,7 +41,12 @@ document.getElementById('new-course').addEventListener('submit', function(e) {
 
     const course = new Course(title,instructor,image);
     console.log(course);
-    
+
+
+    const ui = new UI();
+    ui.addCourseToList(course);
+
+    ui.clearControls();
     
     
     e.preventDefault();
